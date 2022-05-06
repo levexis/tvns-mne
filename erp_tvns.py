3277cd71e492c17354bbbab8eb6d81d80cbb6bfc
Paul Cook <daddydev@daddyswork.com>
2022-05-06 13:12:02 +0200

    Support for detrending=none although detrending is now done in eeg_tvns.

--- erp_tvns.py
+++ erp_tvns.py
@@ -5,6 +5,9 @@ import matplotlib as plt
 import mne
 import numpy as np
 from scipy import stats
+import sys
+import getopt
+
 
 plt.use('Qt5Agg')
 
@@ -13,6 +16,10 @@ TMIN=0
 TMAX=1
 ERP_TMIN=.3
 ERP_TMAX=.5
+TINT=.05
+CHARTS = False
+TOPOMAPS = True
+DETREND = None
 #CHANNELS=['CP3','CP1','CPz','CP2','CP4','P3','P1','Pz','P2','P4','PO3','POz','PO4']
 CHANNELS=['CP1','CPz','CP2','P1','Pz','P2']
 #CHANNELS=['Pz']
@@ -28,9 +35,12 @@ def process_grand():
         print(f'LOADING {file}')
         evoked = mne.read_evokeds(file)
         print(f'datalen: {len(evoked[0]._data[0])} size: {evoked[0]._size} channels: {len(evoked[0].ch_names)} filename: {file}')
-
-        tvns_evokeds.append(evoked[0].pick_channels(CHANNELS))#.detrend(1))
-        sham_evokeds.append(evoked[1].pick_channels(CHANNELS))#.detrend(1))
+        if DETREND is None:
+            tvns_evokeds.append(evoked[0].pick_channels(CHANNELS))
+            sham_evokeds.append(evoked[1].pick_channels(CHANNELS))
+        else:
+            tvns_evokeds.append(evoked[0].pick_channels(CHANNELS)).detrend(DETREND)
+            sham_evokeds.append(evoked[1].pick_channels(CHANNELS)).detrend(DETREND)
         tvns_means.append(evoked[0].copy().crop(ERP_TMIN,ERP_TMAX).data.mean())
         sham_means.append(evoked[1].copy().crop(ERP_TMIN,ERP_TMAX).data.mean())
 
@@ -44,25 +54,66 @@ def process_grand():
         return figure
 
     #plot topomaps for every 50ms
-    times=np.arange(TMIN,TMAX+.05,.05)
-    format_fig(sham_grand.plot_topomap(times),
-               'Sham Topomap by Time',
-               'SHAM Evoked')
-    format_fig(tvns_grand.plot_topomap(times),
-               'tVNS Topomap by Time',
-               'tVNS Evoked')
-
-    #fig, anim = tvns_grand.animate_topomap(
-    #    times=times, ch_type='eeg', frame_rate=2, time_unit='s', blit=False)
-
-
-    fig=mne.viz.plot_compare_evokeds([tvns_grand, sham_grand],picks='eeg', combine='mean',show_sensors=True)
-    format_fig(fig[0],'ERP Comparison',"Orange=Sham, Blue=tVNS")
+    times=np.arange(TMIN,TMAX+TINT,TINT)
+    if CHARTS:
+        if TOPOMAPS:
+            format_fig(sham_grand.plot_topomap(times),
+                       'Sham Topomap by Time',
+                       'SHAM Evoked')
+            format_fig(tvns_grand.plot_topomap(times),
+                       'tVNS Topomap by Time',
+                       'tVNS Evoked')
+        fig=mne.viz.plot_compare_evokeds([tvns_grand, sham_grand],picks='eeg', combine='mean',show_sensors=True)
+        format_fig(fig[0],'ERP Comparison',"Orange=Sham, Blue=tVNS")
 
 
     #PO sites only
+    zeros = np.full((1, 21), 0)[0]
+    t_test = stats.ttest_rel(tvns_means, zeros)
+    print ('tvns means v zeros: ',t_test)
+    t_test = stats.ttest_rel(sham_means, zeros)
+    print ('sham means v zeros: ',t_test)
+
     t_test = stats.ttest_rel(tvns_means,sham_means,alternative='greater')
     print("dependent t-test, tvns > sham:", t_test)
 
+
 if __name__ == '__main__':
+    argv = sys.argv[1:]
+    help_mess="./erp_tvns -c [--charts=n] [--topomaps] [--tmin=0.3] [--tmax=0.5] [--tint=.05] [--channels=Pz,Cz/all]"
+
+    if (len(argv)):
+        CHARTS = False
+        try:
+            opts, args = getopt.getopt(argv, "p:cf:", ["charts=",'tmin=','tmax=','tint=','topomaps','channels='])
+        except:
+            print("invalid command line arguments:")
+            print(help_mess)
+            sys.exit()
+        for opt, arg in opts:
+            if opt == '-h':
+                print(help_mess)
+            elif opt == '-f':
+                L_PASS = int(arg)
+            elif opt == '--tint':
+                T_INT = float(arg)
+            elif opt == '--tmin':
+                T_MIN = float(arg)
+            elif opt == '--tmax':
+                T_MAX = float(arg)
+            elif opt == '--topomaps':
+                TOPOMAPS = True
+            elif opt == '--channels':
+                if arg == 'all':
+                    CHANNELS=[]
+                else:
+                    CHANNELS=arg.split(',')
+            elif opt in ["-c", "--charts"]:
+                if arg == "n":
+                    CHARTS = False
+                else:
+                    CHARTS = True
     process_grand()
+    print(argv)
+
+
--- evoke_tvns.py
+++ evoke_tvns.py
@@ -18,6 +18,8 @@ T_MIN = 0
 T_MAX = 1
 T_INT = .05
 L_PASS = 15
+H_PASS = None
+TOPOMAPS = False
 DETREND = 1 # linear detrend
 # used for charting, evoked contains all channels
 CHANNELS = ['CP1', 'CPz', 'CP2', 'P1', 'Pz', 'P2']
@@ -35,15 +37,20 @@ def format_fig(figure, window_tit='', canvas_tit=''):
 def write_evoked(participant_number,epochs):
 
     # lowpass to 10 for ERP analysis, the lower the filter the longer the data window needs to be
-    shorter_epochs = epochs.copy().filter(None, L_PASS, fir_design='firwin')
+    shorter_epochs = epochs.copy().filter(H_PASS, L_PASS, fir_design='firwin')
 
     ## crop to time period of interest for ERP identification
     shorter_epochs.crop(tmin=T_MIN, tmax=T_MAX, include_tmax=True)
 
 
     ## COMPARE ERPS
-    evoked_tvns = shorter_epochs['tvns'].average()#.detrend(DETREND)
-    evoked_sham = shorter_epochs['sham'].average()#.detrend(DETREND)
+    if DETREND is None:
+        evoked_tvns = shorter_epochs['tvns'].average()#.detrend(DETREND)
+        evoked_sham = shorter_epochs['sham'].average()#.detrend(DETREND)
+    else:
+        evoked_tvns = shorter_epochs['tvns'].average().detrend(DETREND)
+        evoked_sham = shorter_epochs['sham'].average().detrend(DETREND)
+
 
     # save evoked
     mne.write_evokeds(f'out_evoked/evoked_tvns_sham_P{participant.part_str}-ave.fif', [evoked_tvns, evoked_sham])
@@ -55,12 +62,12 @@ if __name__ == '__main__':
     # get options, default is no charts -p6 -c also switches on charts
     argv = sys.argv[1:]
     ALL=False
-    help_mess="evoke_tvns -- -p <participant number> -c [--charts=n] [--tmin=0] [--tmax=1] [--tint=.05] [--detrend=none/0/1"
+    help_mess="evoke_tvns -- -p <participant number> -c -h0.1 -l15 [--charts=n] [--topomaps] [--tmin=0] [--tmax=1] [--tint=.05] [--detrend=none/0/1"
     p_charts = False
     if (len(argv)):
         CHARTS = False
         try:
-            opts, args = getopt.getopt(argv, "p:cf:", ["charts=","all",'tmin=','tmax=','tint=','pcharts','detrend='])
+            opts, args = getopt.getopt(argv, "p:ch:l:", ["charts=","all",'tmin=','tmax=','tint=','pcharts','detrend=','topomaps'])
         except:
             print("invalid command line arguments:")
             print(help_mess)
@@ -68,8 +75,10 @@ if __name__ == '__main__':
         for opt, arg in opts:
             if opt == '-h':
                 print(help_mess)
-            elif opt == '-f':
+            elif opt == '-l,':
                 L_PASS = int(arg)
+            elif opt == '-h':
+                H_PASS = float(arg)
             elif opt == '--tint':
                 T_INT = float(arg)
             elif opt == '--tmin':
@@ -88,6 +97,8 @@ if __name__ == '__main__':
                     CHARTS = False
                 else:
                     CHARTS = True
+            elif opt == '--topomaps':
+                TOPOMAPS = True
             elif opt == '--all':
                 ALL = True
     if ALL:
@@ -125,12 +136,13 @@ if __name__ == '__main__':
         fig = mne.viz.plot_compare_evokeds([grand_diff], picks='eeg', combine='mean')
         format_fig(fig[0],'Difference Wave, Stim-Sham')
 
-        # plot 0 to 1 with 50ms steps (inclusive)
-        timepoints = np.arange(T_MIN, T_MAX + T_INT, T_INT)
-        format_fig(sham_grand.plot_topomap(timepoints),
-                   'SHAM Topomaps',
-                   'SHAM Grand Topomaps')
-        format_fig(tvns_grand.plot_topomap(timepoints),
-                   'tVNS Topomaps',
-                   'tVNS Grand Topomaps')
+        if TOPOMAPS:
+            # plot 0 to 1 with 50ms steps (inclusive)
+            timepoints = np.arange(T_MIN, T_MAX + T_INT, T_INT)
+            format_fig(sham_grand.plot_topomap(timepoints),
+                       'SHAM Topomaps',
+                       'SHAM Grand Topomaps')
+            format_fig(tvns_grand.plot_topomap(timepoints),
+                       'tVNS Topomaps',
+                       'tVNS Grand Topomaps')
         input("Press Enter to exit (closes charts)...")
