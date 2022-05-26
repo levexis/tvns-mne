1ba72f2a3cbd957080c34d2790fd1f477f9a4cd4
Paul Cook <daddydev@daddyswork.com>
2022-05-26 17:48:02 +0200

    Added tshift paratmeter and default to no detrend

--- evoke_tvns.py
+++ evoke_tvns.py
@@ -13,14 +13,15 @@ import glob
 from clean_events import Participant,_CHECKED
 
 SUBJ=1
-CHARTS=True
+CHARTS=False
 T_MIN = 0
 T_MAX = 1
 T_INT = .05
-L_PASS = 15
+L_PASS = None
+TSHIFT = 0
 H_PASS = None
 TOPOMAPS = False
-DETREND = 1 # linear detrend
+DETREND = None # linear detrend
 # used for charting, evoked contains all channels
 CHANNELS = ['CP1', 'CPz', 'CP2', 'P1', 'Pz', 'P2']
 
@@ -44,13 +45,12 @@ def write_evoked(participant_number,epochs):
 
 
     ## COMPARE ERPS
-    if DETREND is None:
-        evoked_tvns = shorter_epochs['tvns'].average()#.detrend(DETREND)
-        evoked_sham = shorter_epochs['sham'].average()#.detrend(DETREND)
-    else:
-        evoked_tvns = shorter_epochs['tvns'].average().detrend(DETREND)
-        evoked_sham = shorter_epochs['sham'].average().detrend(DETREND)
-
+    evoked_tvns = shorter_epochs['tvns'].average()
+    evoked_sham = shorter_epochs['sham'].average()
+    if DETREND is not None:
+        print(f'detrending ({DETREND})')
+        evoked_tvns.detrend(DETREND)
+        evoked_sham.detrend(DETREND)
 
     # save evoked
     mne.write_evokeds(f'out_evoked/evoked_tvns_sham_P{participant.part_str}-ave.fif', [evoked_tvns, evoked_sham])
@@ -62,22 +62,23 @@ if __name__ == '__main__':
     # get options, default is no charts -p6 -c also switches on charts
     argv = sys.argv[1:]
     ALL=False
-    help_mess="evoke_tvns -- -p <participant number> -c -h0.1 -l15 [--charts=n] [--topomaps] [--tmin=0] [--tmax=1] [--tint=.05] [--detrend=none/0/1"
+    help_mess="evoke_tvns -- -p <participant number> -c -h0.1 -l15 [--all] [--charts=n] [--topomaps] [--tmin=0] [--tmax=1] [--tint=.05] [--detrend=none/0/1] [--hpass=0.1] [--lpass=15] [--tshift=0]"
     p_charts = False
     if (len(argv)):
         CHARTS = False
         try:
-            opts, args = getopt.getopt(argv, "p:ch:l:", ["charts=","all",'tmin=','tmax=','tint=','pcharts','detrend=','topomaps'])
+            opts, args = getopt.getopt(argv, "p:c", ["charts=","all",'tmin=','tmax=','tint=','pcharts','detrend=','topomaps','hpass=','lpass=','tshift='])
         except:
             print("invalid command line arguments:")
             print(help_mess)
             sys.exit()
+        print(opts)
         for opt, arg in opts:
             if opt == '-h':
                 print(help_mess)
-            elif opt == '-l,':
+            elif opt == '--lpass':
                 L_PASS = int(arg)
-            elif opt == '-h':
+            elif opt == '--hpass':
                 H_PASS = float(arg)
             elif opt == '--tint':
                 T_INT = float(arg)
@@ -99,6 +100,9 @@ if __name__ == '__main__':
                     CHARTS = True
             elif opt == '--topomaps':
                 TOPOMAPS = True
+            elif opt == '--tshift':
+                TSHIFT = float(arg)
+                print (TSHIFT)
             elif opt == '--all':
                 ALL = True
     if ALL:
@@ -115,6 +119,15 @@ if __name__ == '__main__':
         part_file = f"out_epochs/cleaned_stimoff_epoch_sub-{participant.part_str}-epo.fif"
         print(f"P{subj} loading {part_file}")
         epochs = mne.read_epochs(part_file)
+        print(TSHIFT)
+        if TSHIFT!=0:
+            print(f"shifting time by {TSHIFT} offset")
+            epochs.shift_time(TSHIFT)
+
+            # enable to save epochs with new offset
+            #epochs.save(f'out_epochs/cleaned_stimoff_epoch_sub-{participant.part_str}-epo.fif', overwrite=True)
+
+        print (f"Epochs from {round(epochs.times[0],2)} to {round(epochs.times[-1],2)}")
         total_epochs += len(epochs)
         stim_epochs += len(epochs['tvns'])
         evoked_tvns, evoked_sham = write_evoked(subj,epochs)
@@ -125,15 +138,19 @@ if __name__ == '__main__':
             format_fig(fig[0], f'Paricipant {subj} Evoked Comparison',f'Evoked Comparison Participant: {subj}')
 
     print(f"Processed {total_epochs} epochs from {len(participants)} participants ({(total_epochs*100)/(len(participants)*88)}%) Stim: {stim_epochs}, Sham: {total_epochs-stim_epochs}")
-
+    print(CHARTS)
     if CHARTS:
         tvns_grand = mne.grand_average(tvns_evokeds)
         sham_grand = mne.grand_average(sham_evokeds)
-        fig = mne.viz.plot_compare_evokeds([tvns_grand, sham_grand], picks='eeg', combine='mean')
+        fig = mne.viz.plot_compare_evokeds({'tvns':tvns_grand, 'sham': sham_grand},
+                                           picks='eeg',
+                                           combine='mean',
+                                           colors=dict(tvns='orange',sham='green'),
+                                           linestyles=dict(tvns='solid',sham='dotted'))
         format_fig(fig[0],'Grand Evoked Comparison')
         # difference wave
         grand_diff = mne.combine_evoked([tvns_grand,sham_grand],weights=[1, -1])
-        fig = mne.viz.plot_compare_evokeds([grand_diff], picks='eeg', combine='mean')
+        fig = mne.viz.plot_compare_evokeds({'tVNS - Sham':grand_diff}, picks='eeg', combine='mean')
         format_fig(fig[0],'Difference Wave, Stim-Sham')
 
         if TOPOMAPS:
